8b8165a7b42db71271a6b65810fb34498d1a57d0
Rick Cavalla <monolith32@gmail.com>
2019-08-10 12:09:16 -0400

    Fix sorting in query

--- ELT project - Karen/ETL_Query.sql
+++ ELT project - Karen/ETL_Query.sql
@@ -2,4 +2,4 @@ SELECT esp.sector,
 	to_char(AVG(esp.ytd_percent), '9999999D99%') AS ytd_change
 FROM etl_stock_price as esp
 GROUP BY esp.sector
-ORDER BY ytd_change DESC
+ORDER BY AVG(esp.ytd_percent) DESC
